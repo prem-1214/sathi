add199a574f42055110b02b42b4bf691326d1054
Prem-Rajput <rajputpremkumar22@gmail.com>
2025-04-26 15:07:51 +0530

    minor bugs fix

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+        <div style={{ padding: 20, maxWidth: 600, margin: 'auto' }}>
+            <h1>Welcome to the AI Agent</h1>
+            <p>This is a simple AI agent that can answer your questions.</p>
+            <Link to="/askAI" style={{ textDecoration: 'none', color: 'blue' }}>
+            Go to AI Input Page
+            </Link>
+        </div>
+    </>
   )
 }
 

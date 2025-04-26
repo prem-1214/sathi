62b290680e094caeeae4469fcc8bdb4905bbd548
Prem-Rajput <rajputpremkumar22@gmail.com>
2025-04-26 14:56:30 +0530

    minor bugs fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import AIInputPage from './InputPage'
+import Home from './Home'
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={<Home />} />
           <Route path='/askAI' element={<AIInputPage />} />
         </Routes>
       </BrowserRouter>
